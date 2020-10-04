6ae317c365ac0941e15c8fa35a1d79a2b3743b86
Nevan Wichers <wichersn@google.com>
2020-10-03 18:37:21 -0700

    Fixed test.

--- test.py
+++ test.py
@@ -1,56 +1,57 @@
 import unittest
+import train
+from matplotlib import pyplot as plt
+import numpy as np
 
+from absl import app
+from absl import flags
+FLAGS = flags.FLAGS
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        metric_test_eval_datas = gen_data_batch(10000, 4)
+non_train_indexies = [1,2,3]
 
-        def test_metric(eval_datas, gen_boards, thresh, expected_min, expected_max):
-            for i in range(2):
-                print("eval_datas:")
-                plt_boards(eval_datas[i])
+class TestCase(unittest.TestCase):
+    def setUp(self):
+        FLAGS(['test'])
+        self.metric_test_eval_datas = train.gen_data_batch(2000, 4)
 
-                print("gen_boards:")
-                plt_boards(gen_boards[i])
+    def metric_asserts(self, eval_datas, gen_boards, thresh, expected_min, expected_max):
+        metric_val = train.metric(eval_datas, gen_boards, thresh, non_train_indexies)
 
-                print("=" * 40)
+        self.assertGreaterEqual(metric_val, expected_min)
+        self.assertLessEqual(metric_val, expected_max)
 
-            metric_val = metric(eval_datas, gen_boards, thresh)
-
-            print("metric_val", metric_val)
-
-            assert (metric_val >= expected_min)
-            assert (metric_val <= expected_max)
-
-        print("Same data should be 1 ish, even when reordered")
+    def test_same_data_should_be_1_even_when_reordered(self):
         metric_test_gen_datas = np.stack(
-            [metric_test_eval_datas[:, 0], metric_test_eval_datas[:, 2], metric_test_eval_datas[:, 3],
-             metric_test_eval_datas[:, 1], metric_test_eval_datas[:, 4]], axis=1)
-        test_metric(metric_test_eval_datas, metric_test_gen_datas, .99, 1, 1.1)
+            [self.metric_test_eval_datas[:, 0], self.metric_test_eval_datas[:, 2], self.metric_test_eval_datas[:, 3],
+             self.metric_test_eval_datas[:, 1], self.metric_test_eval_datas[:, 4]], axis=1)
+        self.metric_asserts(self.metric_test_eval_datas, metric_test_gen_datas, .99, 1, 1.1)
 
-        print("Threshold works")
-        assert (metric(metric_test_eval_datas, metric_test_gen_datas, .99) < metric(metric_test_eval_datas,
-                                                                                    metric_test_gen_datas, .4))
+    def test_threshold_works(self):
+        self.assertLess(train.metric(self.metric_test_eval_datas, self.metric_test_eval_datas, .99, non_train_indexies),
+                        train.metric(self.metric_test_eval_datas, self.metric_test_eval_datas, .4, non_train_indexies))
 
-        print("Only gets partial credit if repeating same state")
+    def test_only_gets_partial_credit_if_repeating_same_state(self):
         metric_test_gen_datas = np.stack(
-            [metric_test_eval_datas[:, 0], metric_test_eval_datas[:, 1], metric_test_eval_datas[:, 2],
-             metric_test_eval_datas[:, 2], metric_test_eval_datas[:, 4]], axis=1)
+            [self.metric_test_eval_datas[:, 0], self.metric_test_eval_datas[:, 1], self.metric_test_eval_datas[:, 2],
+             self.metric_test_eval_datas[:, 2], self.metric_test_eval_datas[:, 4]], axis=1)
         # It should get (1 + 1 + .5) / 3 = .83
-        test_metric(metric_test_eval_datas, metric_test_gen_datas, .99, .83, .87)
+        self.metric_asserts(self.metric_test_eval_datas, metric_test_gen_datas, .99, .83, .88)
 
-        print("No credit for start or end state")
+    def test_no_credit_for_start_or_end_state(self):
         metric_test_gen_datas = np.stack(
-            [metric_test_eval_datas[:, 0], metric_test_eval_datas[:, 2], metric_test_eval_datas[:, 0],
-             metric_test_eval_datas[:, 4], metric_test_eval_datas[:, 4]], axis=1)
+            [self.metric_test_eval_datas[:, 0], self.metric_test_eval_datas[:, 2], self.metric_test_eval_datas[:, 0],
+             self.metric_test_eval_datas[:, 4], self.metric_test_eval_datas[:, 4]], axis=1)
         # It should get (1 + 0 + 0) / 3 = .33
-        test_metric(metric_test_eval_datas, metric_test_gen_datas, .99, .33, .4)
+        self.metric_asserts(self.metric_test_eval_datas, metric_test_gen_datas, .99, .33, .4)
 
-        print("Combine metric works")
-        combine_val = combine_metric(metric_test_eval_datas, metric_test_eval_datas, metric_test_gen_datas, .95)
+    def test_combine_metric_works(self):
+        metric_test_gen_datas = np.stack(
+            [self.metric_test_eval_datas[:, 0], self.metric_test_eval_datas[:, 2], self.metric_test_eval_datas[:, 0],
+             self.metric_test_eval_datas[:, 4], self.metric_test_eval_datas[:, 4]], axis=1)
+        combine_val = train.combine_metric(self.metric_test_eval_datas, self.metric_test_eval_datas, metric_test_gen_datas, .95, non_train_indexies)
         print(combine_val)
-        assert (combine_val >= 1)
-        assert (combine_val <= 1.1)
+        self.assertGreaterEqual(combine_val, 1)
+        self.assertLessEqual(combine_val, 1.1)
 
 
 if __name__ == '__main__':
