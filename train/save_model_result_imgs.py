693fec10f33c167e25fb149c13460bf2038adbb0
Nevan Wichers <wichersn@google.com>
2020-10-12 19:48:13 -0700

    Finished saving result images.

--- train/save_model_result_imgs.py
+++ train/save_model_result_imgs.py
@@ -12,19 +12,30 @@ flags.DEFINE_string('job_dir', '',
                     'Root directory for writing logs/summaries/checkpoints.')
 flags.DEFINE_alias('job-dir', 'job_dir')
 
-def save_imgs(input_array_name):
-    input_array_path = os.path.join(FLAGS.job_dir, input_array_name)
+def plt_boards(boards, axes, pos):
+  for i in range(len(boards)):
+    axes[pos, i].imshow(boards[i,:,:,0], interpolation='nearest', cmap=plt.cm.binary)
+    axes[pos, i].axis('off')
 
-    data = np.load(io.gfile.GFile(input_array_path, 'rb'))
-    print("data.shape", data.shape)
+def save_imgs(input_array_names, num_to_save):
+    datas = {}
+    for name in input_array_names:
+        input_array_path = os.path.join(FLAGS.job_dir, name)
+        datas[name] = np.load(io.gfile.GFile(input_array_path, 'rb'))
 
-    # TODO: Should save all of the images
-    imsave(input_array_path+".png", data[0,0,:,:,0])
+    for i in range(num_to_save):
+        fig, axes = plt.subplots(len(input_array_names), len(datas[name][i]))
+        for pos, name in enumerate(input_array_names):
+            plt_boards(datas[name][i], axes, pos)
+        img_file = io.gfile.GFile(os.path.join(FLAGS.job_dir, "imgs", str(i)+".png"), 'wb')
+        plt.savefig(img_file)
 
 def main(_):
-    save_imgs("eval_datas")
-    save_imgs("gen_boards")
-    save_imgs("adver_gen_boards")
+    save_imgs({"eval_datas": "Ground Truth",
+               "gen_boards": "Inferred",
+               "adver_gen_boards": "Inferred with adver loss"},
+              20
+              )
 
 
 if __name__ == '__main__':
