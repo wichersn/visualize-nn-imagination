69c2999c44c6aa52ba8cf09749b0288830e5bdeb
Victor <platers81@gmail.com>
2021-02-14 16:28:04 -0500

    update tests

--- test.py
+++ test.py
@@ -1,12 +1,10 @@
 import unittest
 
 from train import visualize_metric
-from train.train import CountAccuracyInverseMetric
-from matplotlib import pyplot as plt
+from train.train import AccuracyInverseMetric
 import numpy as np
 from train.data_functions import num_black_cells, gen_data_batch, get_batch
 
-from absl import app
 from absl import flags
 FLAGS = flags.FLAGS
 
@@ -17,8 +15,8 @@ class MetricTestCase(unittest.TestCase):
         FLAGS(['test'])
         self.metric_test_eval_datas = gen_data_batch(2000, 4)
 
-    def metric_asserts(self, eval_datas, gen_boards, thresh, expected_min, expected_max):
-        metric_val = visualize_metric.visualize_metric(eval_datas, gen_boards, thresh, non_train_indexies)
+    def metric_asserts(self, eval_datas, gen_boards, expected_min, expected_max):
+        metric_val = visualize_metric.visualize_metric(eval_datas, gen_boards, non_train_indexies)
 
         self.assertGreaterEqual(metric_val, expected_min)
         self.assertLessEqual(metric_val, expected_max)
@@ -27,42 +25,37 @@ class MetricTestCase(unittest.TestCase):
         metric_test_gen_datas = np.stack(
             [self.metric_test_eval_datas[:, 0], self.metric_test_eval_datas[:, 2], self.metric_test_eval_datas[:, 3],
              self.metric_test_eval_datas[:, 1], self.metric_test_eval_datas[:, 4]], axis=1)
-        self.metric_asserts(self.metric_test_eval_datas, metric_test_gen_datas, .99, 1, 1.1)
-
-    def test_threshold_works(self):
-        self.assertLess(
-            visualize_metric.visualize_metric(self.metric_test_eval_datas, self.metric_test_eval_datas, .99, non_train_indexies),
-            visualize_metric.visualize_metric(self.metric_test_eval_datas, self.metric_test_eval_datas, .4, non_train_indexies))
+        self.metric_asserts(self.metric_test_eval_datas, metric_test_gen_datas, 1, 1.1)
 
     def test_only_gets_partial_credit_if_repeating_same_state(self):
         metric_test_gen_datas = np.stack(
             [self.metric_test_eval_datas[:, 0], self.metric_test_eval_datas[:, 1], self.metric_test_eval_datas[:, 2],
              self.metric_test_eval_datas[:, 2], self.metric_test_eval_datas[:, 4]], axis=1)
-        # It should get (1 + 1 + .5) / 3 = .83
-        self.metric_asserts(self.metric_test_eval_datas, metric_test_gen_datas, .99, .83, .88)
+        # It should get less than 1 since 2 states are the same
+        self.metric_asserts(self.metric_test_eval_datas, metric_test_gen_datas, .65, .90)
 
     def test_no_credit_for_start_or_end_state(self):
         metric_test_gen_datas = np.stack(
-            [self.metric_test_eval_datas[:, 0], self.metric_test_eval_datas[:, 2], self.metric_test_eval_datas[:, 0],
+            [self.metric_test_eval_datas[:, 0], self.metric_test_eval_datas[:, 0], self.metric_test_eval_datas[:, 0],
              self.metric_test_eval_datas[:, 4], self.metric_test_eval_datas[:, 4]], axis=1)
-        # It should get (1 + 0 + 0) / 3 = .33
-        self.metric_asserts(self.metric_test_eval_datas, metric_test_gen_datas, .99, .33, .4)
+        # It should get close to 0
+        self.metric_asserts(self.metric_test_eval_datas, metric_test_gen_datas, .0, .4)
 
     def test_combine_metric_works(self):
         metric_test_gen_datas = np.stack(
             [self.metric_test_eval_datas[:, 0], self.metric_test_eval_datas[:, 2], self.metric_test_eval_datas[:, 0],
              self.metric_test_eval_datas[:, 4], self.metric_test_eval_datas[:, 4]], axis=1)
-        combine_val = visualize_metric.combine_metric(self.metric_test_eval_datas, self.metric_test_eval_datas, metric_test_gen_datas, .95, non_train_indexies)
+        combine_val = visualize_metric.combine_metric(self.metric_test_eval_datas, self.metric_test_eval_datas, metric_test_gen_datas, non_train_indexies)
         print(combine_val)
         self.assertGreaterEqual(combine_val, 1)
         self.assertLessEqual(combine_val, 1.1)
 
-class CountAccuracyInverseMetricTestCase(unittest.TestCase):
+class AccuracyInverseMetricTestCase(unittest.TestCase):
     def setUp(self):
         FLAGS(['test'])
 
     def test(self):
-      metric = CountAccuracyInverseMetric()
+      metric = AccuracyInverseMetric(FLAGS.board_size)
       y_pred = np.array([21.3, 10.6, 70.8, 90, 35.2]) / (FLAGS.board_size **2)
       y_true = np.array([21, 10, 70, 97, 35]) / (FLAGS.board_size **2)
       metric.update_state(y_true, y_pred)
--- train/visualize_metric.py
+++ train/visualize_metric.py
@@ -7,7 +7,6 @@ def single_index_metric(gt, gen):
   0 is no correlation, 1 is perfect
   """
   mccs = np.ones(len(gen))
-  print(gt.shape, gen.shape)
   for i in range(len(gen)):
     mcc = matthews_corrcoef(gt[i].flatten(), gen[i].flatten()>.5)
     mccs[i] = abs(mcc)
@@ -17,23 +16,28 @@ def single_index_metric(gt, gen):
 
 def single_gen_index_metric(gt_boards, gen, non_train_indexies):
   """Metric representing how well a single generated state matches the true states.
-  Only counts the closest match
+  Only counts the true states that match the best
   """
   metrics_for_gt = []
   for j in non_train_indexies:
     metrics_for_gt.append(single_index_metric(gt_boards[:, j], gen))
-  return max(metrics_for_gt)
+  return max(metrics_for_gt), np.argmax(metrics_for_gt)
 
 
-def visualize_metric(eval_datas, gen_boards, thresh, non_train_indexies):
-  """Averages the metric on each of the generated states."""
+def visualize_metric(eval_datas, gen_boards, non_train_indexies):
+  """Averages the metric on each of the generated states. Each generated states is matched to a unique true state"""
   total_metric = 0
+  used_idxs = []
   for i in non_train_indexies:
-    total_metric += single_gen_index_metric(eval_datas, gen_boards[:, i], non_train_indexies)
+    metric_val, idx = single_gen_index_metric(eval_datas, gen_boards[:, i], non_train_indexies)
+    if idx not in used_idxs:
+      total_metric += metric_val
+      used_idxs.append(idx)
+
   return total_metric / float(len(non_train_indexies))
 
 
-def combine_metric(eval_datas, gen_boards, adver_gen_boards, thresh, non_train_indexies):
-  adver_metric = visualize_metric(eval_datas, adver_gen_boards, thresh, non_train_indexies)
-  regular_metric = visualize_metric(eval_datas, gen_boards, thresh, non_train_indexies)
+def combine_metric(eval_datas, gen_boards, adver_gen_boards, non_train_indexies):
+  adver_metric = visualize_metric(eval_datas, adver_gen_boards, non_train_indexies)
+  regular_metric = visualize_metric(eval_datas, gen_boards, non_train_indexies)
   return max(adver_metric, regular_metric)
